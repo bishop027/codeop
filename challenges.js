87380c07522c94e997a9d964b31cc219f997f584
bishop027 <57153219+bishop027@users.noreply.github.com>
2021-02-15 05:42:53 +0100

    Initial commit

---
+++ challenges.js
@@ -0,0 +1,71 @@
+/*------------------
+1)
+Write a function that determines
+if a number is a palindrome. A number is a
+palindrome if it reads the same forwards and
+backwards. 
+
+Note:
+- All negative numbers should return false.
+- Single numbers should return true.
+
+Test cases:
+console.log(isPalindrome(12321)); // true
+console.log(isPalindrome(-12321)); // false
+console.log(isPalindrome(1)); // true
+console.log(isPalindrome(84146)); // false
+console.log(isPalindrome(12)); // false
+------------------*/
+
+function isPalindrome(num) {}
+
+/*------------------
+2)
+Create a function that takes a grid
+as an argument and adds all the numbers
+in that grid together.
+
+Examples:
+const y = [[[1], [2]]];
+console.log(addGridItems(y)); // 3
+
+const x = [[[1, 0, -3], [2, 4], [3]], [[4, 4, 3], [5, -3, -1], [6, 10]], [[7, -5, -5], [8, 0], [9, 1]]];
+console.log(addGridItems(x)); // 50
+------------------*/
+
+function addGridItems(grid) {}
+
+/*------------------
+3)
+Create a function that turns a string into a
+file name: all letters are lowercase, spaces are
+swapped for underscores, and it includes a
+file extension. 
+
+This function should take two parameters: the string, 
+and the type of file it is (so you know what file
+extension to add). You only need to cover
+the extensions below:
+- PDF: .pdf
+- image: .png
+- JSON: .json
+
+Examples:
+console.log(createFileName("Sofia Engineering Resume", "PDF")); // "sofia_engineering_resume.pdf"
+console.log(createFileName("Sofia Profile Photo", "image")); // "sofia_profile_photo.png"
+console.log(createFileName("Data from Pokemon API", "JSON")); // "data_from_pokemon_api.json"
+------------------*/
+
+function createFileName() {}
+
+/*------------------
+4) Print the following pattern
+99 96 93 90 88
+99 96 93 90
+99 96 93
+99 96
+99
+
+Hints:
+- Use a loop within a loop.
+------------------*/
